fa49a368da2033f9ab4732fbba2b23005fda2126
c8e4 <c8e4@proton.me>
2024-10-02 14:42:40 +0200

    numbers

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 export const MAP_SIZE = 16000; // Dota 2-like map size
 export const HERO_SIZE = 100; // Hero size in Dota 2 units
 export const TEXTURE_REPEAT = 10; // How many times the texture is repeated over ground
-export const AMBIENT_LIGHT_INTENSITY = 0.6;
+export const AMBIENT_LIGHT_INTENSITY = 0.0;
 export const DIRECTIONAL_LIGHT_INTENSITY = 0.8;
 export const DIRECTIONAL_LIGHT_POSITION = { x: 10, y: 20, z: 10 }; // Initial position
 export const CAMERA_LIGHT_OFFSET = { x: 10, y: 10, z: 10 }; // Offset behind the camera
@@ -25,5 +25,5 @@ export const CAMERA_DEFAULT_TARGET = { x: HERO_STARTING_POSITION.x, y: HERO_SIZE
 
 // Scale multiplier for bot models (1 = 100% of HERO_SIZE)
 export const HERO_SCALE = { x: 1, y: 1, z: 1 };
-export const HERO_MOVE_SPEED = 0.7; // Speed of hero movement per frame
+export const HERO_MOVE_SPEED = 3.7; // Speed of hero movement per frame
 export const HERO_ROTATION_SPEED = 0.05; // Speed of hero rotation
